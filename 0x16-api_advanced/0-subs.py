fcdf6d741017ddadd25e140013cca14d4a1778bf
Ladydee3 <delisiletwala62@gmail.com>
2024-06-04 17:42:55 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,25 +5,26 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
+    if not subreddit or not isinstance(subreddit, str):
+        return 0
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an HTTPError on bad HTTP status codes
-    except requests.exceptions.HTTPError:
-        return 0
-    except requests.exceptions.RequestException as e:
-        # Log the exception if needed
-        print(f"An error occurred: {e}")
+        if response.status_code != 200:
+            return 0
+    except requests.RequestException:
         return 0
 
     try:
         results = response.json().get("data")
-        return results.get("subscribers", 0)  # Default to 0 if 'subscribers' key is missing
+        if results:
+            return results.get("subscribers", 0)
+        return 0
     except (ValueError, AttributeError):
-        # ValueError for JSON decoding issues, AttributeError if 'results' is None
         return 0
 
 
