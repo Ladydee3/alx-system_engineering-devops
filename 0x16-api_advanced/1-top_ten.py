3c9a63e1bf19ae954aebb02d979d05e9776b644c
Ladydee3 <delisiletwala62@gmail.com>
2024-06-04 16:30:54 +0000

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
-"""
-
+""" Exporting csv files"""
+import json
 import requests
+import sys
+
 
 def top_ten(subreddit):
-    url = "(link unavailable)".format(subreddit)
-    headers = {"User-Agent": "Custom"}
-    params = {"limit": 10}
-
-    try:
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return
-
-    data = response.json()
-    if "data" in data and "children" in data["data"]:
-        for post in data["data"]["children"]:
-            title = post["data"]["title"]
-            print(title)
+    """Read reddit API and return top 10 hotspots """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        list_titles = r.json()['data']['children']
+        for a in list_titles[:10]:
+            print(a['data']['title'])
     else:
-        print("No posts found")
+        return(print("None"))
 
