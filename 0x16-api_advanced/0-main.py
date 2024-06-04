348830d0ef67d6082717600a682d6e70b308d0f6
Ladydee3 <delisiletwala62@gmail.com>
2024-06-04 21:04:25 +0000

    0-subs.py

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+"""
+0-main
+"""
 import sys
-from subs import number_of_subscribers
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
-        print("Usage: ./0-main.py <subreddit>")
+        print("Please pass an argument for the subreddit to search.")
     else:
-        subreddit = sys.argv[1]
-        print("{:d}".format(number_of_subscribers(subreddit)))
-
-
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,16 @@
+#!/usr/bin/python3
+"""Function to query subscribers on given Reddit subreddit."""
 import requests
 
-def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json().get("data")
-            if data:
-                return data.get("subscribers", 0)
-            else:
-                print("No data found in the response.")
-                return 0
-        else:
-            print("Error: Received status code {}".format(response.status_code))
-            return 0
-    except requests.exceptions.RequestException as e:
-        print("Request failed: {}".format(e))
-        return 0
 
+def number_of_subscribers(subreddit):
+    """Return total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
